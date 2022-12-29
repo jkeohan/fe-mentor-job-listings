f0463f555b1840d3eb2f37de2f60bae0e8563169
Joe Keohan <jkeohan@gmail.com>
2022-12-29 15:29:34 -0500

    added hover states and box shadow

--- src/components/JobListing/JobListing.tsx
+++ src/components/JobListing/JobListing.tsx
@@ -3,6 +3,7 @@ import { Tags } from './components/Tags';
 import * as logos from '../../assets/images/';
 import './styles.scss';
 import { JobListingObj } from '../../types/JobListingObj';
+import { ReactSVG } from 'react-svg';
 
 interface Props {
   jobsData: JobListingObj[];
@@ -12,11 +13,27 @@ interface Props {
 export function JobListing({ jobsData, addFilter }: Props) {
   const jobListingsMarkup = jobsData.map((job, index) => {
     const tags = [job.role, job.level, ...job.languages, ...job.tools];
+    const svgPath = `./assets/images/${job.logo}`;
 
     return (
-      <article className="job-listing p-4 pt-0 text-left" key={job.id}>
+      <article
+        className="job-listing p-4 pt-0 text-left rounded-md shadow-xl shadow-desaturated-dark-cyan/30"
+        key={job.id}
+      >
         {/* <ReactSVG src={svgPath} /> */}
         <img src={logos.photosnap} alt="logo" className="w-12 " />
+        {/* <ReactSVG
+          src={svgPath}
+          // style={svgStyle}
+          // className="w-[50px] h-[50px]"
+          beforeInjection={(svg) => {
+            // svg.classList.add('svg-class-name');
+            // svg.setAttribute('style', 'viewBox = “0 0 100 100"');
+            svg.setAttribute('style', 'width: 50px!important');
+            // const [firstGElement] = [...svg.querySelectorAll('g')];
+            // firstGElement.setAttribute('fill', 'blue');
+          }}
+        /> */}
         <div>
           <div className="flex items-center gap-[10px] my-2">
             <div className="text-xs pr-5 text-desaturated-dark-cyan font-bold">
@@ -28,12 +45,14 @@ export function JobListing({ jobsData, addFilter }: Props) {
               </div>
             )}
             {job.featured && (
-              <div className="text-sm p-2 leading-tight bg-black text-white rounded-full">
+              <div className="text-sm p-2 leading-none bg-black text-white rounded-full">
                 FEATURED
               </div>
             )}
           </div>
-          <div className="text-sm">{job.position}</div>
+          <div className="text-sm hover:text-desaturated-dark-cyan hover:cursor-pointer">
+            {job.position}
+          </div>
           <div className="flex items-center gap-[10px] my-2 text-dark-grayish-cyan">
             <div>{job.postedAt}</div>
             <span>&middot; </span>
@@ -42,9 +61,7 @@ export function JobListing({ jobsData, addFilter }: Props) {
             <div>{job.location}</div>
           </div>
         </div>
-
         <hr className="my-2" />
-
         <Tags tags={tags} addFilter={addFilter} />
       </article>
     );
--- src/components/SearchFilter/SearchFilter.tsx
+++ src/components/SearchFilter/SearchFilter.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import removeButton from '../../assets/images/remove-icon.svg';
-import './styles.scss';
 
 interface Props {
   filters: string[];
@@ -10,27 +9,31 @@ interface Props {
 
 export function SearchFilter({ filters, clearFilter, removeFilter }: Props) {
   const activeFilters = filters.map((filter, index) => (
-    <div className="filter-item flex" key={filter}>
-      <div className="filter flex items-center leading-none py-1 px-2 bg-light-grayish-cyan-background text-desaturated-dark-cyan rounded-t-md rounded-b-md font-bold">
+    <div className="flex" key={filter}>
+      <div className="flex items-center leading-none py-1 px-2 bg-light-grayish-cyan-background text-desaturated-dark-cyan rounded-l-md font-bold">
         {filter}
       </div>
-      <span className="close">
+      <div className="cursor-pointer text-white bg-desaturated-dark-cyan hover:bg-black rounded-r-md py-[7px] px-[5px]">
         <img
           className="w-6 h-6"
           src={removeButton}
           alt="mobile-header"
           onClick={() => removeFilter(filter)}
         />
-      </span>
+      </div>
     </div>
   ));
 
   return (
-    <div className="search-filter flex bg-white p-4">
+    <div className="flex bg-white p-4 w-[327px] m-auto relative">
       <div className="flex-wrap flex gap-[15px] grow">{activeFilters}</div>
-      <div onClick={clearFilter} className="clear flex">
+      <div
+        onClick={clearFilter}
+        className="flex items-center text-dark-grayish-cyan hover:text-desaturated-dark-cyan hover:cursor-pointer"
+      >
         Clear
       </div>
     </div>
   );
 }
+
