f546a8fe0b360249b3aa35f7f45c88771eb8e841
Joe Keohan <jkeohan@gmail.com>
2023-01-03 08:02:12 -0500

    adjusted css for spacing between filter and job

--- src/App.tsx
+++ src/App.tsx
@@ -91,7 +91,7 @@ function App() {
           removeFilter={handleRemoveFilter}
         />
       </div>
-      <section className="pt-12 main-content md:pt-14 w-[327px] m-auto md:m-w-[90%] md:w-3/4">
+      <section className="py-12 main-content md:pt-14 w-[327px] m-auto md:m-w-[90%] md:w-3/4">
         <section className="job-listings flex flex-col gap-10 md:gap-4">
           {filterListings.length > 0 ? (
             <JobListing jobsData={filterListings} addFilter={handleAddFilter} />
--- src/components/JobListing/JobListing.tsx
+++ src/components/JobListing/JobListing.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Tags } from './components/Tags';
+import {Tags} from './components/Tags'
 import { JobListingObj } from '../../types/JobListingObj';
 
 interface Props {
@@ -10,17 +10,17 @@ interface Props {
 export function JobListing({ jobsData, addFilter }: Props) {
   const jobListingsMarkup = jobsData.map((job, index) => {
     const tags = [job.role, job.level, ...job.languages, ...job.tools];
-    // const svgPath = `./assets/images/${job.logo}`;
-    // https://create-react-app.dev/docs/using-the-public-folder#adding-assets-outside-of-the-module-system
-    const svgLogoPath = `${process.env.PUBLIC_URL}/assets/images/${job.logo}`;
-    console.log({ svgLogoPath });
 
     return (
       <article
         className="job-listing bg-white p-4 pt-0 md:pt-4 text-left rounded-md shadow-xl shadow-desaturated-dark-cyan/30 md:flex md:items-center md:justify-center md:gap-5"
         key={job.id}
       >
-        <img src={`../assets/images/${job.logo}`} alt="logo" className="-mt-[25px] md:mt-0 w-12 md:w-[80px] md:h-[80px]"/>
+        <img
+          src={`../assets/images/${job.logo}`}
+          alt="logo"
+          className="-mt-[25px] md:mt-0 w-12 md:w-[80px] md:h-[80px]"
+        />
         <div>
           <div className="flex items-center gap-[10px] my-2">
             <div className="text-xs md:text-base pr-5 md:pr-2.5 text-desaturated-dark-cyan font-bold">
@@ -42,9 +42,9 @@ export function JobListing({ jobsData, addFilter }: Props) {
           </div>
           <div className="flex-center gap-[10px] my-2 text-dark-grayish-cyan">
             <div>{job.postedAt}</div>
-            <span>&middot; </span>
+            <span className="scale-150 opacity-75">&middot; </span>
             <div>{job.contract}</div>
-            <span>&middot;</span>
+            <span className="scale-150 opacity-75">&middot;</span>
             <div>{job.location}</div>
           </div>
         </div>
