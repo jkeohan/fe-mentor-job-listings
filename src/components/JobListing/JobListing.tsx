50be37fa35f20afe7a55f7d8530796704d526dba
Joe Keohan <jkeohan@gmail.com>
2023-01-03 14:49:48 -0500

    resolved issue with netlify deployment...issues was with nesting Tags inside components/Tags folder...needed to lift Tags.tsx into components folder

--- src/components/JobListing/JobListing.tsx
+++ src/components/JobListing/JobListing.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Tags} from './components/Tags'
+import {Tags} from './components'
 import { JobListingObj } from '../../types/JobListingObj';
 
 interface Props {
