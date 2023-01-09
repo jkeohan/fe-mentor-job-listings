f9a6132e03e1e9154b48c38379deff0f4881bae9
Joe Keohan <jkeohan@gmail.com>
2023-01-09 17:54:35 -0500

    added 4 tests to Tags comp

--- src/utilities/transformJobListing.ts
+++ src/utilities/transformJobListing.ts
@@ -1,7 +1,6 @@
 import type { Job, JobTransformed } from '../types/JobListing';
 
 export function transformJobListing(data: Job[]): JobTransformed[] {
-  console.log({ data });
   return data.map((job: Job) => {
     const { position, role, languages, tools, featured } = job;
     return {
