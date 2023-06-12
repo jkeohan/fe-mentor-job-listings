b0410205ebb8df3ba7cd08a16e44659cc66b2cf4
Joe Keohan <jkeohan@gmail.com>
2023-06-11 21:00:35 -0400

    updated storybook for tailwind

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,13 @@
+// import '!style-loader!css-loader!postcss-loader!tailwindcss/tailwind.css';
+// added this to integrate tailwindcss into storybook
+import '../src/App.scss'
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-}
+      date: /Date$/
+    }
+  }
+};
